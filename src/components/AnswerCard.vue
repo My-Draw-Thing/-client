bb4ccda9c77ed5add5eacb111a45fd3fa1c2325d
whyt <will.heryanto@gmail.com>
2019-09-20 16:22:22 +0700

    will-2

--- src/components/AnswerCard.vue
+++ src/components/AnswerCard.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="btn flex items-center justify-center py-1">
-    <button class="inner-btn h-40 my-1 flex justify-center items-center focus:outline-none" :class="color">
-      <p class="text-white font-bold text-2xl antialiased">Answer</p>
+    <button
+      @click="submitAnsware(answare)"
+      class="inner-btn h-40 my-1 flex justify-center items-center focus:outline-none"
+      :class="color"
+    >
+      <p class="text-white font-bold text-2xl antialiased">{{ answare }}</p>
     </button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['color']
+  props: ['color', 'answare'],
+  methods: {
+    submitAnsware(answare) {
+      if (answare === this.game.questions.correct) {
+        let getLocalToken = JSON.parse(localStorage.getItem('token'))
+
+        quizilla.doc(this.roomID).update({
+          corrects: firebase.firestore.FieldValue.arrayUnion(getLocalToken)
+        })
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 .red {
-  background-color: #E4443C;
+  background-color: #e4443c;
 }
 
 .blue {
